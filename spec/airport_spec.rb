49d3bfae87eab54503136de44c0935dcf3075c9a
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-04-19 12:31:32 +0100

    Refactored airport spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,11 +8,15 @@ describe Airport do
   let(:weather) { double 'weather' }
   subject { described_class.new(weather: weather) }
 
+  before do
+    allow(weather).to receive(:stormy?).and_return(false)
+    allow(plane).to receive(:land)
+    allow(plane).to receive(:takeoff)
+  end
+
   describe '.landing' do
     before do
-      allow(weather).to receive(:stormy?).and_return(false)
-      allow(plane).to receive(:grounded?).and_return(false)
-      allow(plane).to receive(:land)
+      allow(plane).to receive(:grounded?)
     end
 
     it "receives a landing plane" do
@@ -36,7 +40,6 @@ describe Airport do
     end
 
     it "prevents takeoff when weather is stormy" do
-      allow(plane).to receive(:takeoff)
       expect { subject.takeoff(plane) }.to raise_error("Too stormy.")
     end
 
@@ -48,39 +51,36 @@ describe Airport do
 
   describe '.takeoff' do
     before do
-      allow(weather).to receive(:stormy?).and_return(false)
       allow(plane).to receive(:grounded?).and_return(false)
-      allow(plane).to receive(:takeoff)
-      allow(plane).to receive(:land)
+      subject.land(plane)
+      allow(plane).to receive(:grounded?).and_return(true)
     end
 
     it "plane takes off and is gone from airport" do
-      subject.land(plane)
-      allow(plane).to receive(:grounded?).and_return(true)
       subject.takeoff(plane)
       expect(subject.planes).not_to include plane
+    end
+
+    it "instructs a plane to take off" do
+      expect(plane).to receive(:takeoff)
+      subject.takeoff(plane)
     end
   end
 
   describe '#edge cases' do
-    before do
-      allow(weather).to receive(:stormy?) { false }
-      allow(plane).to receive(:takeoff)
-    end
-
     it "planes can only take off from airports they are in" do
       allow(plane).to receive(:grounded?) { true }
       expect { subject.takeoff(plane) }.to raise_error('Plane not in airport.')
     end
 
-    it "planes in air cannot take off" do
-      allow(plane).to receive(:grounded?) { false }
-      expect { subject.takeoff(plane) }.to raise_error('Plane in air.')
-    end
-
     it "planes already in any airport cannot land" do
       allow(plane).to receive(:grounded?) { true }
       expect { subject.land(plane) }.to raise_error('Plane grounded elsewhere.')
+    end
+
+    it "planes in air cannot take off" do
+      allow(plane).to receive(:grounded?) { false }
+      expect { subject.takeoff(plane) }.to raise_error('Plane in air.')
     end
   end
 end
